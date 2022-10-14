bf5fb9dbf1374adbd5e04d77dd9bf6b502070fb0
USER <USER@L05.r1805.tciii.org.tw>
2022-10-14 08:46:49 +0800

    init

---
+++ MyJava/src/tw/brad/myjava/Brad03.java
@@ -0,0 +1,15 @@
+package tw.brad.myjava;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		byte a = 10, b = 3;
+		System.out.println(a);
+		System.out.println(b);
+		int c;
+		c = a + b;	// int
+		System.out.println(c);
+		// brad@brad.tw, bradtw
+	}
+
+}
